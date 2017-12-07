338344be032fe71174be4cb52e3a2e4e9ea69eb8
Carlos <carlos@lisnr.com>
2017-12-07 10:11:39 -0500

    added elb

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "instance" {
 resource "aws_launch_configuration" "example" {
     image_id        = "ami-40d28157"
     instance_type   = "t2.micro"
-    security_groups = ["${aws_security_group.elb.id}"]
+    security_groups = ["${aws_security_group.instance.id}"]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -47,10 +47,11 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
     availability_zones   = ["${data.aws_availability_zones.all.names}"]
-    
+
     load_balancers    = ["${aws_elb.example.name}"]
     health_check_type = "ELB"
 
+    
     min_size = 2
     max_size = 10
 
@@ -62,45 +63,40 @@ resource "aws_autoscaling_group" "example" {
 }
 
 resource "aws_elb" "example" {
-    name               = "terraform-elb-example"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
-    security_groups    = ["${aws_security_group.elb.id}"]
+    name = "terraform-elb-example"
+    availability_zones   = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
     
     listener {
         lb_port           = 80
         lb_protocol       = "http"
         instance_port     = "${var.server_port}"
         instance_protocol = "http"
-    } 
-
+    }
+    
     health_check {
-        healthy_threshold   = 2
-        unhealthy_threshold = 2
-        timeout             = 3
-        interval            = 30
-        target              = "HTTP:${var.server_port}/"
+        healthy_threshold     = 2
+        unhealthy_threshold   = 2
+        timeout               = 3
+        interval              = 30
+        target                = "HTTP:${var.server_port}/"
     }
 }
 
 resource "aws_security_group" "elb" {
-    name = "terraform-example-elb"
+    name = "terraform-asg-example"
     
     ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
+        from_port  = 80
+        to_port    = 80
+        protocol   = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
+        from_port  = 0
+        to_port    = 0
+        protocol   = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
-    lifecycle {
-        create_before_destroy = true
-    }
 }
-
