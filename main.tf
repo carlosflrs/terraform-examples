0b0137ea23ebc84b9091c3b43e824c56a919021d
Carlos <carlos@lisnr.com>
2017-12-06 23:14:02 -0500

    removed single ec2 instance and public ip variable

--- main.tf
+++ main.tf
@@ -7,28 +7,7 @@ variable "server_port" {
     default = 8080
 }
 
-output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
-}
-
-
 data "aws_availability_zones" "all" {}
-
-resource "aws_instance" "example" {
-    ami                    = "ami-40d28157"
-    instance_type           = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World" > index.html
-                nohup busybox httpd -f -p "${var.server_port}" &
-                EOF
-
-    tags {
-        Name = "terraform-example"
-    }
-}
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
